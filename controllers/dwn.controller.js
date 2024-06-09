66ffcc6a9804acfb2bd40df586c58090cbcb46bf
dayan-cabrera <dayanrcabreragarcia@gmail.com>
2024-06-09 13:35:02 -0400

    trying again

--- controllers/dwn.controller.js
+++ controllers/dwn.controller.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const { Router, response } = express;
-const { DonwloadService } = require("../services/dwn.service");
+const { DownloadService } = require("../services/dwn.service");
 
 class DownloadController {
   download = async (req = express.request, res = response) => {
     const { url } = req.query;
     console.log(decodeURIComponent(url));
-    const dwnService = new DonwloadService(decodeURIComponent(url));
+    const dwnService = new DownloadService(decodeURIComponent(url));
     const files = await dwnService.getFiles();
 
     res.status(200).json({ files: files });
--- services/dwn.service.js
+++ services/dwn.service.js
@@ -2,64 +2,79 @@ const axios = require("axios");
 const cheerio = require("cheerio");
 const { saveAs } = require("file-saver");
 
-class DonwloadService {
-  constructor(folderUrl) {
-    this.folderUrl = folderUrl;
-    this.axios = axios;
-  }
-
-  async getHtml() {
-    try {
-      const { data: html } = await this.axios.get(`${this.folderUrl}`);
-      return html;
-    } catch (err) {
-      throw new Error(`Failed to fetch HTML: ${err}`);
-    }
-  }
-
-  getFileNameFromUrl(url) {
-    let fileName = decodeURI(url).replace(/%20|%5d/g, " ");
-    return fileName;
+class DownloadService {
+  constructor(url) {
+    this.url = url;
   }
 
   async getFiles() {
-    try {
-      const imagesUrl = [];
-      await this.getHtml().then((res) => {
-        const $ = cheerio.load(res);
+    const axiosResponse = await axios.request({
+      method: "GET",
+      url: this.url,
+    });
 
-        $("a[href$='.jpg']").each((index, element) => {
-          const link = $(element).attr("href") || "";
-          if (link) {
-            imagesUrl.push({
-              name: this.getFileNameFromUrl(link),
-              url: `${this.folderUrl}${link}`,
-            });
-          }
-        });
-      });
-      for (const file of imagesUrl) {
-        await this.downloadElement(file);
-      }
-      return imagesUrl;
-    } catch (error) {
-      console.error(`Error fetching image names: ${error}`);
-      return error;
-    }
-  }
-
-  async downloadElement(file) {
-    try {
-      const response = await this.axios.get(file.url, {
-        responseType: "arraybuffer",
-      });
-
-      // Descargar el archivo directamente en el navegador
-      saveAs(new Blob([response.data], { type: "image/jpeg" }), file.name);
-    } catch (error) {
-      console.error("Error al descargar el elemento:", error);
-    }
+    return axiosResponse.data;
   }
 }
 
-module.exports = { DonwloadService };
+// class DonwloadService {
+//   constructor(folderUrl) {
+//     this.folderUrl = folderUrl;
+//     this.axios = axios;
+//   }
+
+//   async getHtml() {
+//     try {
+//       const { data: html } = await this.axios.get(`${this.folderUrl}`);
+//       return html;
+//     } catch (err) {
+//       throw new Error(`Failed to fetch HTML: ${err}`);
+//     }
+//   }
+
+//   getFileNameFromUrl(url) {
+//     let fileName = decodeURI(url).replace(/%20|%5d/g, " ");
+//     return fileName;
+//   }
+
+//   async getFiles() {
+//     try {
+//       const imagesUrl = [];
+//       await this.getHtml().then((res) => {
+//         const $ = cheerio.load(res);
+
+//         $("a[href$='.jpg']").each((index, element) => {
+//           const link = $(element).attr("href") || "";
+//           if (link) {
+//             imagesUrl.push({
+//               name: this.getFileNameFromUrl(link),
+//               url: `${this.folderUrl}${link}`,
+//             });
+//           }
+//         });
+//       });
+//       for (const file of imagesUrl) {
+//         await this.downloadElement(file);
+//       }
+//       return imagesUrl;
+//     } catch (error) {
+//       console.error(`Error fetching image names: ${error}`);
+//       return error;
+//     }
+//   }
+
+//   async downloadElement(file) {
+//     try {
+//       const response = await this.axios.get(file.url, {
+//         responseType: "arraybuffer",
+//       });
+
+//       // Descargar el archivo directamente en el navegador
+//       saveAs(new Blob([response.data], { type: "image/jpeg" }), file.name);
+//     } catch (error) {
+//       console.error("Error al descargar el elemento:", error);
+//     }
+//   }
+// }
+
+module.exports = { DownloadService };

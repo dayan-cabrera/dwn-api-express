68568b774447c02a778c340945b9db100eb9397d
dayan-cabrera <dayanrcabreragarcia@gmail.com>
2024-06-08 21:36:45 -0400

    Decoding the url

--- controllers/dwn.controller.js
+++ controllers/dwn.controller.js
@@ -5,7 +5,7 @@ const { DonwloadService } = require("../services/dwn.service");
 class DownloadController {
   download = async (req = express.request, res = response) => {
     const { url } = req.query;
-    const dwnService = new DonwloadService(url);
+    const dwnService = new DonwloadService(decodeURIComponent(url));
     const files = await dwnService.getFiles();
 
     res.status(200).json({ files: files });

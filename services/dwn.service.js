06bbcdc66bfb0084334e83be9df34e6ccd019c47
dayan-cabrera <dayanrcabreragarcia@gmail.com>
2024-06-09 11:14:19 -0400

    trying whit chatgpt option

--- services/dwn.service.js
+++ services/dwn.service.js
@@ -1,12 +1,11 @@
 const axios = require("axios");
 const cheerio = require("cheerio");
-const { JSDOM } = require("jsdom");
 
 const path = require("path");
 const os = require("os");
 const fs = require("fs");
 
-class DonwloadService {
+class DownloadService {
   constructor(folderUrl) {
     this.folderUrl = folderUrl;
     this.axios = axios;
@@ -14,10 +13,14 @@ class DonwloadService {
 
   async getHtml() {
     try {
-      const { data: html } = await this.axios.get(`${this.folderUrl}`);
+      const { data: html } = await this.axios.get(`${this.folderUrl}`, {
+        httpsAgent: new (require("https").HttpsAgent)({
+          rejectUnauthorized: false,
+        }), // Opcional: Ignora certificados SSL/TLS inválidos para pruebas
+      });
       return html;
     } catch (err) {
-      throw new Error(`Failed to fetch HTML: ${err}`);
+      throw new Error(`Failed to fetch HTML: ${err.message}`);
     }
   }
 
@@ -29,18 +32,17 @@ class DonwloadService {
   async getFiles() {
     try {
       const imagesUrl = [];
-      await this.getHtml().then((res) => {
-        const $ = cheerio.load(res);
+      const html = await this.getHtml();
+      const $ = cheerio.load(html);
 
-        $("a[href$='.jpg']").each((index, element) => {
-          const link = $(element).attr("href") || "";
-          if (link) {
-            imagesUrl.push({
-              name: this.getFileNameFromUrl(link),
-              url: `${this.folderUrl}${link}`,
-            });
-          }
-        });
+      $("a[href$='.jpg']").each((index, element) => {
+        const link = $(element).attr("href") || "";
+        if (link) {
+          imagesUrl.push({
+            name: this.getFileNameFromUrl(link),
+            url: `${this.folderUrl}${link}`,
+          });
+        }
       });
       for (const file of imagesUrl) {
         await this.downloadElement(file);
@@ -48,9 +50,10 @@ class DonwloadService {
       return imagesUrl;
     } catch (error) {
       console.error(`Error fetching image names: ${error}`);
-      return false;
+      return error;
     }
   }
+
   async downloadElement(file) {
     try {
       const response = await this.axios.get(file.url, {
@@ -71,4 +74,4 @@ class DonwloadService {
   }
 }
 
-module.exports = { DonwloadService };
+module.exports = { DownloadService };

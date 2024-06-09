94d24b9131e3436f57840ea7d56ae9af85804316
dayan-cabrera <dayanrcabreragarcia@gmail.com>
2024-06-09 14:17:01 -0400

    trying again v1: fixing location

--- services/dwn.service.js
+++ services/dwn.service.js
@@ -14,6 +14,7 @@ class DownloadService {
       headers: {
         "User-Agent":
           "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36",
+        "X-Forwarded-For": "200.80.80.80",
       },
     });
     const $ = cheerio.load(axiosResponse.data);

4a814e5217bf298837976c810fc30b071e19c4d7
dayan-cabrera <dayanrcabreragarcia@gmail.com>
2024-06-09 13:37:53 -0400

    trying again v1

--- services/dwn.service.js
+++ services/dwn.service.js
@@ -11,6 +11,10 @@ class DownloadService {
     const axiosResponse = await axios.request({
       method: "GET",
       url: this.url,
+      headers: {
+        "User-Agent":
+          "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36",
+      },
     });
 
     return axiosResponse.data;
